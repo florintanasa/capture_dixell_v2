f18371aea96fd1d61c9a2822b7fbfffc95a98a5d
Florin Tanasa <florin.tanasa@gmail.com>
2024-04-19 12:25:32 +0300

    added more info

--- capture_dixell.sh
+++ capture_dixell.sh
@@ -2,9 +2,12 @@
 P=0
 # run infinite loop
 while true ; do
-  # read some variables from file variable_command exp. 'start|5|192.168.1.15'
-  # with: echo "stop|5|192.168.1.15" > variable_command
-  # you can stop the script
+  # read some variables from file variable_command, example 'start|5|192.168.1.15'
+  # was written with command: echo "stop|5|192.168.1.15" > variable_command
+  # where with 'stop' word you can stop the script
+  # where with 'start' word you can run the script
+  # next after first delimiter | it's a number for how many seconds to wait, a very small number can flood the device
+  # next after second delimiter | it's ip address for Dixell Xweb300D device
     while IFS="|" read -r RUN TIME IP < variable_command; do
       # check if stop and exit from the loop
         if [ "$RUN" = 'stop' ]; then
